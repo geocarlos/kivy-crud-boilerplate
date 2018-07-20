40cf89cecef98cb2d1b9f1f4378f90c5cded1416
Geocarlos Alves <geowildcat@gmail.com>
2018-07-20 10:25:49 -0400

    Create basic view code

---
+++ app.py
@@ -0,0 +1,9 @@
+"""
+    You may change you app main class name. Once, or if, you do so, you
+    should adjust the name here accordingly.
+"""
+
+from view.view import MyApp
+
+if __name__ == '__main__':
+    MyApp().run()
---
+++ view/view.py
@@ -0,0 +1,6 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(text='Hello, Kivy!')
