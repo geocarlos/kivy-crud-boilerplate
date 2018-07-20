db57219376239d9527874d5e6be23f4dabd2582f
Geocarlos Alves <geowildcat@gmail.com>
2018-07-20 15:53:09 -0400

    Add kv file and screens

--- view/view.py
+++ view/view.py
@@ -5,7 +5,14 @@
 
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+Builder.load_file('./view/MyApp.kv')
+
+class AppScreenManager(ScreenManager):
+    pass
 
 class MyApp(App):
     def build(self):
-        return Button(text='Hello, Kivy!')
+        return AppScreenManager()
