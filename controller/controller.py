62e66b828060a362836fd67ad302bff63b62961b
Geocarlos Alves <geowildcat@gmail.com>
2018-07-20 12:08:14 -0400

    Create basic controller code

---
+++ controller/controller.py
@@ -0,0 +1,53 @@
+"""
+    The code in this file connects the model and the view.
+"""
+
+from model.model import Item, Category, ItemObj
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# ----------------------------------------------------------------------
+
+"""
+    Connect to our SQLite database and return a Session object
+"""
+engine = create_engine("sqlite:///model/db/app.db", echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# ----------------------------------------------------------------------
+
+"""
+    Add a new item to the Item table. Data must be in a dictionary.
+"""
+def add_item(data):
+    item = Item()
+    item.name = data['name']
+    item.cat_id = data['cat_id']
+    session.add(item)
+    session.commit()
+    session.close()
+
+"""
+    Add a new category to the Category table. Data must be in a dictionary.
+"""
+def add_category(data):
+    category = Category()
+    category.name = data['name']
+    session.add(category)
+    session.commit()
+    session.close()
+
+"""
+    Get everything from tables Item and Category and create a list of ItemObj
+"""
+def get_items():
+    items = session.query(Item).all()
+    cats = session.query(Category).all()
+    itemObjs = []
+    for item in items:
+        cat = [c for c in cats if c.id == item.id][0]
+        itemObj = ItemObj(item, cat)
+        itemObjs.append(itemObj)
+    session.close()
+    return itemObjs
--- model/model.py
+++ model/model.py
@@ -3,7 +3,7 @@
 """
 
 from sqlalchemy import Table, Column, create_engine
-from sqlalchemy import ForeignKey, String, Unicode
+from sqlalchemy import ForeignKey, Integer, String, Unicode
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
@@ -18,10 +18,10 @@ Base = declarative_base(engine)
 
 class ItemObj(object):
 
-    def __init__(self, item, category, *args):
-        super(EntryObj, self).__init__(*args)
-        self.id = entry.id
-        self.item = item.name
+    def __init__(self, item, category):
+        super(ItemObj, self).__init__()
+        self.id = item.id
+        self.name = item.name
         self.category = category
 
 
@@ -35,10 +35,7 @@ class Item(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True)
     name = Column(String(80), nullable=False)
-    category_id = Column(Integer, ForeignKey('category.id'))
+    cat_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-
-
-engine = create_engine('sqlite:///catalog.db')
 
 Base.metadata.create_all(engine)
