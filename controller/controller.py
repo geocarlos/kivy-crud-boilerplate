4d9c8ddf0b8f6c66a0c5c67810aa1fe15531aa5c
geocarlos <geowildcat@gmail.com>
2026-07-23 22:47:28 -0300

    feat: add uv and kivymd

--- controller/controller.py
+++ controller/controller.py
@@ -2,57 +2,33 @@
     The code in this file connects the model and the view.
 """
 
-from model.model import Item, Category, ItemObj
-from sqlalchemy import create_engine
+from model.model import Item, Category, ItemObj, engine
 from sqlalchemy.orm import sessionmaker
 
-# ----------------------------------------------------------------------
-
-"""
-    Connect to our SQLite database and return a Session object
-"""
-engine = create_engine("sqlite:///model/db/app.db", echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# ----------------------------------------------------------------------
 
-"""
-    Add a new item to the Item table. Data must be in a dictionary.
-"""
 def add_item(data):
-    item = Item()
-    item.name = data['name']
-    item.cat_id = data['cat_id']
+    item = Item(name=data['name'], cat_id=data['cat_id'])
     session.add(item)
     session.commit()
-    session.close()
 
-"""
-    Add a new category to the Category table. Data must be in a dictionary.
-"""
+
 def add_category(data):
-    category = Category()
-    category.name = data['name']
+    category = Category(name=data['name'])
     session.add(category)
     session.commit()
-    session.close()
 
-"""
-    Get everything from tables Item and Category and create a list of ItemObj
-"""
+
 def get_items():
     items = session.query(Item).all()
     cats = session.query(Category).all()
-    itemObjs = []
-    for item in items:
-        cat = [c for c in cats if c.id == item.cat_id][0]
-        itemObj = ItemObj(item, cat)
-        itemObjs.append(itemObj)
-    session.close()
-    return itemObjs
+    return [
+        ItemObj(item, next(c for c in cats if c.id == item.cat_id))
+        for item in items
+    ]
+
 
 def get_categories():
-    categories = session.query(Category).all()
-    session.close()
-    return categories
+    return session.query(Category).all()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from kivy-crud-boilerplate!")
+
+
+if __name__ == "__main__":
+    main()
